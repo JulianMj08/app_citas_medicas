20d94aba2eb842a1b041e9579658488b4552e9de
Julian Gomez <julianmjadi@gmail.com>
2025-01-15 02:25:43 +0100

    organizando display_errors para ver errores

--- public/index.php
+++ public/index.php
@@ -3,7 +3,10 @@
 <script src="/js/bootstrap.bundle.min.js"></script>
 
 <?php
-session_start();
+//session_start();
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 require_once __DIR__ . '/../app/router/Route.php';
 require_once __DIR__ . '/../app/router/routes.php';
 require_once __DIR__ . '/../app/models/NoticeAdminModel.php';
